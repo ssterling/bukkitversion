76ce3110018b81958327f5cb072f2cad37e2669b
Seth Price <ssterling@firemail.cc>
2022-07-20 18:08:32 -0400

    Add server version test
    Required upgrade to MockBukkit dependency

---
+++ src/test/java/net/ssterling/bukkitversion/ServerVersionTest.java
@@ -0,0 +1,67 @@
+/*
+ * BukkitVersion - utilities for Minecraft server version info
+ *
+ * Copyright 2022 Seth Price
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+package net.ssterling.bukkitversion;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.bukkit.Bukkit;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+
+/**
+ * The {@code ServerVersionTest} test ensures that the value returned by
+ * {@code Bukkit#getBukkitVersion} is correctly parsed as a valid Bukkit API
+ * version string.
+ *
+ * @author Seth Price
+ */
+class ServerVersionTest
+{
+	private ServerMock server;
+
+	@BeforeEach
+	public void setUp()
+	{
+		server = MockBukkit.mock();
+	}
+
+	@AfterEach
+	public void tearDown()
+	{
+		MockBukkit.unmock();
+	}
+
+	@Test
+	void testServerVersion()
+	{
+		String version_string = Bukkit.getBukkitVersion();
+		BukkitVersion bukkit_version = new BukkitVersion(version_string);
+	}
+}
